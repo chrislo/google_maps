e4e58fed6ce6e2764ebd0ec93e8aa33c3646efaf
Chris Lowis <chris.lowis@gmail.com>
2008-12-21 14:30:41 +0000

    Test map centering for one and two markers

--- test/google_map_test.rb
+++ test/google_map_test.rb
@@ -6,6 +6,10 @@ class GoogleMapTest < Test::Unit::TestCase
   # Replace this with your real tests.
   def setup
     @map = GoogleMap.new
+    @marker = GoogleMapMarker.new(:map => @map, 
+                                  :lat => 40, 
+                                  :lng => -100,
+                                  :html => 'Test Marker')
   end
 
   def test_new_map_has_empty_markers
@@ -13,12 +17,24 @@ class GoogleMapTest < Test::Unit::TestCase
   end
 
   def test_add_markers
-    @map.markers << GoogleMapMarker.new(:map => @map, 
-                                        :lat => 47.6597, 
-                                        :lng => -122.318,
-                                        :html => 'My House')
+    @map.markers << @marker
     assert_equal @map.markers.length, 1
-    assert @map.to_html.include? "google_map_marker_1 = new GMarker(new GLatLng(47.6597, -122.318));"
+    assert @map.to_html.include? "google_map_marker_1 = new GMarker(new GLatLng(40, -100));"
+  end
+  
+  def test_center_on_markers_function_for_empty_map
+    assert @map.center_on_markers_function_js.include? "google_map.setCenter(new GLatLng(0, 0), 0);"
   end
 
+  def test_center_on_markers_function_for_one_marker
+    @map.markers << @marker
+    assert @map.center_on_markers_function_js.include? "new GLatLngBounds(new GLatLng(40, -100), new GLatLng(40, -100))"
+  end
+
+  def test_center_on_markers_function_for_two_markers
+    @map.markers << @marker
+    @map.markers << GoogleMapMarker.new(:map => @map, :lat => 40, :lng => 100, :html => 'Test Marker')
+    assert @map.center_on_markers_function_js.include? "new GLatLngBounds(new GLatLng(40, -100), new GLatLng(40, 100))"
+  end
+  
 end

820731f331948f506b8d28c1c6d4a544753e1e67
Chris Lowis <chris.lowis@gmail.com>
2008-12-21 14:04:55 +0000

    Added a test helper and a couple of basic tests

--- test/google_map_test.rb
+++ test/google_map_test.rb
@@ -1,8 +1,21 @@
-require 'test/unit'
+require File.dirname(__FILE__) + '/test_helper'
 
 class GoogleMapTest < Test::Unit::TestCase
   # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def setup
+    @map = GoogleMap.new
   end
+
+  def test_new_map_has_empty_markers
+    assert @map.markers.empty?
+  end
+
+  def test_add_markers
+    @map.markers << GoogleMapMarker.new(:map => @map, 
+                                        :lat => 47.6597, 
+                                        :lng => -122.318,
+                                        :html => 'My House')
+    assert_equal @map.markers.length, 1
+  end
+
 end
